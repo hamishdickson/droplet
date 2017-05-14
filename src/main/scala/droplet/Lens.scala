eca672bb3d58e50a268d158879835a7669074525
hamishdickson <hamish.dickson@gmail.com>
2017-05-14 11:37:19 +0100

    add some docs

--- src/main/scala/droplet/Lens.scala
+++ src/main/scala/droplet/Lens.scala
@@ -1,10 +1,48 @@
 package droplet
 package lens
 
+/**
+  * Lens
+  *
+  * Given an `A` and a value `B` a `Lens` will let you
+  *
+  *  - get: Given an `A`, get a `B` out of it
+  *
+  *  - set: take an `A` and a `B` you want to set into it, and build you a new `A`
+  *
+  * Examples:
+  *
+  *  {{{
+     scala> import droplet.lens._
+     import droplet.lens._
+
+     scala> case class Foo(s: String, i: Int)
+     defined class Foo
+
+     scala> val fooSLens = Lens[Foo, String] (
+     | (f: Foo) => f.s,
+     | (s: String, f: Foo) => Foo(s, f.i)
+     | )
+     fooSLens: droplet.lens.Lens[Foo,String] = Lens($$Lambda$3862/2088200542@567fa392,$$Lambda$3863/1495144578@7afe75b9)
+
+     scala> val myFoo = Foo("Hello", 1)
+     myFoo: Foo = Foo(Hello,1)
+
+     scala> fooSLens.get(myFoo)
+     res0: String = Hello
+
+     scala> fooSLens.set("World!", myFoo)
+     res1: Foo = Foo(World!,1)
+  *  }}}
+  *
+  */
 case class Lens[A, B](
+  // given an A get a B out of it
   g: A => B,
+  // take an A and a B you want to set into it, and build a new A
   s: (B, A) => A
 ) {
   def get(a: A): B = g(a)
   def set(b: B, a: A): A = s(b, a)
 }
+
--- src/main/scala/droplet/laws/Laws.scala
+++ src/main/scala/droplet/laws/Laws.scala
@@ -9,7 +9,7 @@ import cats.implicits._
 
 /*
  * Quote Ed Kmett
- * 
+ *
  * for some lens l
  *
  * l.get(l.set(b, a)) === b
@@ -17,9 +17,15 @@ import cats.implicits._
  * l.set(l.get(a), a) === a
  */
 trait LensLaws {
+  /**
+    * If I set some field in a to b, then I get that field, I should get b
+    */
   def getAndSetLaw[A, B](l: Lens[A, B], a: A, b: B)(implicit eq: Eq[B]) =
     l.get(l.set(b, a)) === b
 
+  /**
+    * If I set some field and then set it again, I should be able to get the latest thing
+    */
   def compositionLaw[A, B, C](
     lab: Lens[A, B],
     lac: Lens[A, C],
@@ -31,6 +37,9 @@ trait LensLaws {
   ) =
     lac.set(c, lab.set(b, a)) === lac.set(c, a)
 
+  /**
+    * If I set the value a and then get it back, then I don't change the meaning of a
+    */
   def setAndGetLaw[A](l: Lens[A, A], a: A)(implicit eq: Eq[A]) =
     l.set(l.get(a), a) === a
 }
